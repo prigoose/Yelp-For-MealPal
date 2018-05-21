36fc61eb1446e7fb43bd2d25d8192ca0e99a4d6b
Priya Ghose <priyaghose@Priyas-MacBook-Air.local>
2018-05-21 10:30:16 -0700

    Working on getting live data with componentDidMount. Note that with the current commit, everything is broken

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+const port = 3000 //?
+
+// express.static() build/static?
+
+app.listen(port);
+
--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-'use strict';
 import React, { Component } from 'react';
 import './App.css';
 import { apiKey } from './apiKey.js';
 
 const yelp = require('yelp-fusion');
+const client = yelp.client(apiKey);
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       yelpRestaurants: [
-        {restaurant: 'The Melt', rating: 3.5, url: 'https://www.yelp.com/biz/the-melt-san-francisco-9?osq=the+melt'},
-        {restaurant: 'Farmerbrown', rating: 3.5, url: 'https://www.yelp.com/biz/farmerbrown-san-francisco-2'}
+        {name: 'The Melt', rating: 3.5, url: 'https://www.yelp.com/biz/the-melt-san-francisco-9?osq=the+melt'},
+        {name: 'Farmerbrown', rating: 3.5, url: 'https://www.yelp.com/biz/farmerbrown-san-francisco-2'}
       ]
     }
   }
 
   componentDidMount() {
     var mealpalRestaurants = [
-      {restaurant: 'The Melt', address: '925 Market St. , San Francisco, CA'},
-      {restaurant: 'Fresh Roll', address: '157 4th St., San Francisco, CA '}
+      {name: 'Chipotle', address: '126 New Montgomery, San Francisco, CA'},
+      {name: 'Buckhorn Grill', address: '845 Market St, San Francisco, CA'}
     ];
     // for final version, start with an empty mealpalRestaurants array and push in restaurants that I find on page
+    // MUST MOVE GET REQUEST INTO SERVER
+    // YOUTUBE API DOES NOT CURRENTLY SUPPORT CLIENT-SIDE DATA REQUESTS
+    mealpalRestaurants.forEach(function({name, address}) {
+      const searchRequest = {
+        term: name,
+        location: address
+      };
+      client.search(searchRequest).then(response => {
+        const firstResult = response.jsonBody.businesses[0];
+        const prettyJson = JSON.stringify(firstResult, ['id'], 4);
+        console.log(prettyJson);
+      }).catch(e => {
+        console.log('error is: ', e);
+      });
+    })
   }
 
 
   render() {
-    const restaurantListings = this.state.restaurants.map(({restaurant, rating, url}, index) =>
+    const restaurantListings = this.state.yelpRestaurants.map(({restaurant, rating, url}, index) =>
       <li key={index}><a href={url} target="_blank">{restaurant}</a> {rating} stars</li>
     );
     return (
